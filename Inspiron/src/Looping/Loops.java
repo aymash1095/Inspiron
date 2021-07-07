8046fe8860f0dad15e8f80031e97cba6552a3144
aymash1095 <86283541+aymash1095@users.noreply.github.com>
2021-07-07 16:02:15 +0530

    Looping

--- Inspiron/src/Looping/Loops.java
+++ Inspiron/src/Looping/Loops.java
@@ -4,7 +4,7 @@ public class Loops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("saalim ");
+         System.out.println("saalim hussain");
 	}
 
 }
