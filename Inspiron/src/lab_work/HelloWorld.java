8fa4d7a42c200042f064f971c193803122ee1c0c
aymash1095 <86283541+aymash1095@users.noreply.github.com>
2021-07-07 14:33:36 +0530

    Print a number Program

--- Inspiron/src/lab_work/HelloWorld.java
+++ Inspiron/src/lab_work/HelloWorld.java
@@ -1,11 +1,19 @@
 package lab_work;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("HelloWorld");
         System.out.println("Hello Program");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Inspiron Reg number");
+        int a=scan.nextInt();
+        System.out.println("Enter Your Name");
+        String b =scan.next();
+        System.out.println("your reg No is and name is : "+a+" "+b);
 	}
 
 }
