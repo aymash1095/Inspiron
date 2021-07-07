54fade5c58beaa7b712b980f9f13bf20fee1b0df
aymash1095 <86283541+aymash1095@users.noreply.github.com>
2021-07-07 15:35:38 +0530

    Print Number

--- Inspiron/src/lab_work/HelloWorld.java
+++ Inspiron/src/lab_work/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld ");
         System.out.println("Hello Program");
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Inspiron Reg number");
