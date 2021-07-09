a4fc9f9cb13d73ee20693bce3051a658456b5b22
aymash1095 <86283541+aymash1095@users.noreply.github.com>
2021-07-09 23:10:15 +0530

    User Input SD-Arrray

--- Inspiron/src/hit/day_09_07_2021/Array.java
+++ Inspiron/src/hit/day_09_07_2021/Array.java
@@ -1,5 +1,7 @@
 package hit.day_09_07_2021;
 
+import java.util.Scanner;
+
 public class Array {
 public static void main(String[] args) {
 	int a[]= new int[3];
@@ -8,6 +10,22 @@ public static void main(String[] args) {
 	a[2]=8;
 	for(int i=0;i<a.length;i++) {
 		System.out.println(a[i]);
+	}
+      
+	
+	
+	// User Input Single Dimension Array
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter How Many Names Do You Want to print");
+	int n=scan.nextInt();
+	String b[]= new String[n];
+	
+	for(int i=0;i<b.length;i++) {
+		System.out.println("Enter Name "+(i+1)+" :");
+		b[i]=scan.next();
+	}
+	for(int i=0;i<b.length;i++) {
+		System.out.println("Name "+(i+1)+" is : "+b[i]);
 	}
 	
 }
