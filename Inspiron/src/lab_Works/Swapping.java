d6de389108a6bf21b010f81931662b13523f142b
aymash1095 <86283541+aymash1095@users.noreply.github.com>
2021-07-12 09:22:09 +0530

    Swapping

---
+++ Inspiron/src/lab_Works/Swapping.java
@@ -0,0 +1,20 @@
+package lab_Works;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int temp;
+		System.out.println("Before Swapping value of a " + a);
+		System.out.println("Before Swapping value of b " + b);
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("After Swapping value of a " + a);
+		System.out.println("After Swapping value of b " + b);
+
+	}
+
+}
