70a52f984582ac6cd7d1253b2713d85964a549e5
aymash1095 <86283541+aymash1095@users.noreply.github.com>
2021-07-12 09:26:49 +0530

    12_07_2021

---
+++ Inspiron/src/hitdaily_12_07_2021/Java_daily.java
@@ -0,0 +1,10 @@
+package hitdaily_12_07_2021;
+
+public class Java_daily {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello");
+	}
+
+}
