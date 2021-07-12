e4026c0a3e9dd4184dc9d5a508b72d9142f1afd3
aymash1095 <86283541+aymash1095@users.noreply.github.com>
2021-07-12 23:44:49 +0530

    WhileLoop

---
+++ Inspiron/src/lab_Works/WhileLoop.java
@@ -0,0 +1,17 @@
+package lab_Works;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int sum=0;
+        int n=0;
+        while(n<10) { 
+        	sum=sum+n;
+        	n++;
+        	System.out.println(sum);
+        }
+        
+	}
+
+}
